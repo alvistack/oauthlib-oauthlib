050f0da38f9ff854f2230ac9c596244cb36b9879
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-02-16 12:45:43 +0800

    alvistack/v3.2.2
        git clean -xdf
        tar zcvf ../python-oauthlib_3.2.2.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-oauthlib.spec ../python-oauthlib_3.2.2-1.spec
        cp ../python*-oauthlib*3.2.2*.{gz,xz,spec,dsc} /osc/home\:alvistack/oauthlib-oauthlib-3.2.2/
        rm -rf ../python*-oauthlib_3.2.2*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ signals_require = ['blinker>=1.4.0']
 
 setup(
     name='oauthlib',
-    version=oauthlib.__version__,
+    version='3.2.2',
     description='A generic, spec-compliant, thorough implementation of the OAuth request-signing logic',
     long_description=fread('README.rst'),
     long_description_content_type='text/x-rst',
@@ -33,7 +33,7 @@ setup(
     url='https://github.com/oauthlib/oauthlib',
     platforms='any',
     license='BSD',
-    packages=find_packages(exclude=('docs', 'tests', 'tests.*')),
+    packages=find_packages(exclude=('docs', 'tests', 'tests.*', 'examples')),
     python_requires='>=3.6',
     extras_require={
         'rsa': rsa_require,
